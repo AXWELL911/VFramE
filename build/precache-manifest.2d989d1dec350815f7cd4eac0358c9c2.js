2b3c349c3d2578e39671861527e508bfe2dd5d2a
DIWAKAR JOSHI <diwakarjoshi991@gmail.com>
2020-06-09 12:18:34 +0530

    Add files via upload

---
+++ build/precache-manifest.2d989d1dec350815f7cd4eac0358c9c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd70efc1854b0fe03841610e4c6b94af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aa4379735f861feedd7",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "31b0d4c4a3fbf929ebbe",
+    "url": "/static/css/main.2e2786c7.chunk.css"
+  },
+  {
+    "revision": "5aa4379735f861feedd7",
+    "url": "/static/js/2.fcea0c2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fcea0c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31b0d4c4a3fbf929ebbe",
+    "url": "/static/js/main.54c2a16e.chunk.js"
+  },
+  {
+    "revision": "e8f4ef401e16c603e871",
+    "url": "/static/js/runtime-main.f16e1747.js"
+  }
+]);
